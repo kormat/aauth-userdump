8c08e83fdacf91dd9817d82993cd1f9b5b50cdec
Stephen Shirley <kormat@gmail.com>
2018-05-26 11:15:43 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aauth-userdump',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple exporter of chars and their groups',
     url='https://github.com/kormat/aauth-userdump',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
